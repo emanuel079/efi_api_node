b603fee5bdd84074244aecdf08c9c697f23ea838
Agustin-Invaldi <agustin.invaldi@emprinet.com>
2024-09-23 12:30:15 -0300

    first-commit

---
+++ src/controllers/userController.js
@@ -0,0 +1,24 @@
+const User = require('../models/user');
+
+const createUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const user = await User.create({ name, email, password, role });
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error); // Imprime el error en la consola
+    res.status(500).json({ error: 'Error creando el usuario' });
+  }
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error); // Imprime el error en la consola
+    res.status(500).json({ error: 'Error obteniendo los usuarios' });
+  }
+};
+
+module.exports = { createUser, getUsers };
